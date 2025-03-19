9d619f537ace5e5cb0a5235d3069cbea0f9302d1
Tom Paßberg <passberg@rhrk.uni-kl.de>
2025-03-19 15:07:27 +0100

    test update

--- examples/cancel.rs
+++ examples/cancel.rs
@@ -3,7 +3,7 @@ use std::{thread::sleep, time::Duration};
 use parallel_worker::{State, Worker, check_if_cancelled};
 
 fn main() {
-    let worker = Worker::new(worker_function);
+    let worker = Worker::with_num_threads(4, worker_function);
 
     worker.add_task(());
     worker.add_task(());
@@ -14,13 +14,14 @@ fn main() {
     worker.add_task(());
     worker.cancel_tasks();
 
-    worker.get_vec_blocking();
+    
+    assert_eq!(worker.get_blocking(), None);
 }
 
 fn worker_function(_task: (), state: &State) -> Option<()> {
     for _ in 0.. {
         check_if_cancelled!(state);
-        sleep(Duration::from_secs(1));
+        sleep(Duration::from_millis(100));
     }
     return Some(());
 }
