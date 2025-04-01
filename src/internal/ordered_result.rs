1f37a551ef4b30286a24d2cd4400b9183c77c41a
Tom Paßberg <passberg@rhrk.uni-kl.de>
2025-04-01 12:03:01 +0200

    structure

---
+++ src/internal/ordered_result.rs
@@ -0,0 +1,24 @@
+pub struct OrderedResult<T> {
+    pub result: T,
+    pub indx: usize,
+}
+
+impl<T> PartialOrd for OrderedResult<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        other.indx.partial_cmp(&self.indx)
+    }
+}
+
+impl<T> Ord for OrderedResult<T> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.indx.partial_cmp(&self.indx).unwrap()
+    }
+}
+
+impl<T> Eq for OrderedResult<T> {} 
+
+impl<T> PartialEq for OrderedResult<T> {
+    fn eq(&self, other: &Self) -> bool {
+        other.indx == self.indx
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use parallel_worker::prelude::*;
 
 fn main() {
     let worker = OrderedWorker::new(|n: u64| {
-        std::thread::sleep(std::time::Duration::from_secs(n % 5));
-        n
+        fib(n % 49)
     });
 
+    let start = std::time::Instant::now();
     worker.add_task(0);
     worker.add_task(1);
     worker.add_task(2);
@@ -15,8 +15,20 @@ fn main() {
     worker.add_tasks(6..=10);
     worker.add_tasks(11..=20);
     worker.add_tasks(21..=1000);
+    worker.add_tasks(1001..=100000);
 
+    let mut s = 0;
     for result in worker.get_iter_blocking() {
         println!("Result: {}", result);
+        s += result;
     }
+    println!("Time: {:?}", start.elapsed());
+    println!("Sum: {}", s);
+}
+
+fn fib(n: u64) -> u64 {
+    if n <= 1 {
+        return n;
+    }
+    fib(n - 1) + fib(n - 2)
 }
